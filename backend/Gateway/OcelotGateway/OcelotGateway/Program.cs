9214b31c121b9383c4265910a3a983a48de12919
trungdq3305 <fccdotm1@gmail.com>
2025-09-07 15:25:39 +0700

    config ocelot

--- backend/Gateway/OcelotGateway/OcelotGateway/Program.cs
+++ backend/Gateway/OcelotGateway/OcelotGateway/Program.cs
@@ -1,25 +1,15 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// ??c c?u hình ocelot.json
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddOcelot();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+await app.UseOcelot();   // Ocelot vào pipeline ??u
 
 app.Run();
