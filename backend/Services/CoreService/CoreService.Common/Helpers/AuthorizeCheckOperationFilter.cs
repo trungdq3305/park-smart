79c05127d60bfc96aab6ac445252a648b77088f4
trungdq3305 <fccdotm1@gmail.com>
2025-09-07 17:24:42 +0700

    common

---
+++ backend/Services/CoreService/CoreService.Common/Helpers/AuthorizeCheckOperationFilter.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreService.Common.Helpers
+{
+
+    public class AuthorizeCheckOperationFilter : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            var hasAuthorize = context.MethodInfo
+                .GetCustomAttributes(true)
+                .OfType<AuthorizeAttribute>()
+                .Any() ||
+                context.MethodInfo.DeclaringType
+                .GetCustomAttributes(true)
+                .OfType<AuthorizeAttribute>()
+                .Any();
+
+            if (hasAuthorize)
+            {
+                operation.Security = new List<OpenApiSecurityRequirement>
+            {
+                new OpenApiSecurityRequirement
+                {
+                    [ new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        }
+                    ] = new string[] { }
+                }
+            };
+            }
+        }
+    }
+}
