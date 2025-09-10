5e7027c368c715eb4c7e127fd61073328e4908a9
PhamVietHoang <hoangpvse183123@fpt.edu.vn>
2025-09-10 22:11:28 +0700

    Add _id to Ward schema and handle empty wards list
    Introduces an explicit _id field to the Ward schema using mongoose ObjectId and enforces wardName uniqueness. Updates WardService to throw NotFoundException if no wards are found, improving error handling and response consistency.

--- backend/Services/parking-service/src/module/ward/schemas/ward.schema.ts
+++ backend/Services/parking-service/src/module/ward/schemas/ward.schema.ts
@@ -1,9 +1,15 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
-import { Document } from 'mongoose'
+import mongoose from 'mongoose'
 
 @Schema() // Không sử dụng _id cho Ward
-export class Ward extends Document {
-  @Prop()
+export class Ward {
+  @Prop({
+    type: mongoose.Schema.Types.ObjectId,
+    default: () => new mongoose.Types.ObjectId(),
+  })
+  _id: mongoose.Schema.Types.ObjectId
+
+  @Prop({ required: true, unique: true })
   wardName: string
 }
 
