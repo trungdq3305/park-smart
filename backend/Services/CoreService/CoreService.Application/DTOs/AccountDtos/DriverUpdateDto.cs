2c716cdf56e636c915dc87d0a1718bccc84cdca9
trungdq3305 <fccdotm1@gmail.com>
2025-09-19 14:31:08 +0700

    thêm point vào token, update postman put all roles-bỏ email

--- backend/Services/CoreService/CoreService.Application/DTOs/AccountDtos/DriverUpdateDto.cs
+++ backend/Services/CoreService/CoreService.Application/DTOs/AccountDtos/DriverUpdateDto.cs
@@ -9,9 +9,6 @@ namespace CoreService.Application.DTOs.AccountDtos
 {
     public class DriverUpdateDto
     {
-        [EmailAddress(ErrorMessage = "Email không hợp lệ")]
-        public string Email { get; set; } = string.Empty;
-
         [Phone(ErrorMessage = "Số điện thoại không hợp lệ")]
         [RegularExpression(@"^0\d{9}$", ErrorMessage = "Số điện thoại phải có 10 chữ số và bắt đầu bằng số 0")]
         public string PhoneNumber { get; set; }
