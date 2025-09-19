148ece11db955afebb7cc9050eaae16662a0ec18
trungdq3305 <fccdotm1@gmail.com>
2025-09-19 20:29:35 +0700

    comment, faq repo

---
+++ backend/Services/CoreService/CoreService.Repository/Interfaces/IFaqStatusRepository.cs
@@ -0,0 +1,14 @@
+﻿using CoreService.Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreService.Repository.Interfaces
+{
+    public interface IFaqStatusRepository
+    {
+        Task<FaqStatus?> GetByNameAsync(string statusName);
+    }
+}
