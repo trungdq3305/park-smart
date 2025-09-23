63010ac16f2f8198e6a45cbb0fe4c07f26989c27
trungdq3305 <fccdotm1@gmail.com>
2025-09-23 12:59:29 +0700

    validate pass

--- backend/Services/CoreService/CoreService.Application/DTOs/AuthDtos/ConfirmForgotRequest.cs
+++ backend/Services/CoreService/CoreService.Application/DTOs/AuthDtos/ConfirmForgotRequest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,9 @@ namespace CoreService.Application.DTOs.AuthDtos
     public class ConfirmForgotPassRequest
     {
         public string Email { get; set; } = string.Empty;
+        [MinLength(8, ErrorMessage = "Mật khẩu phải có ít nhất 8 ký tự")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$",
+            ErrorMessage = "Mật khẩu phải chứa ít nhất một chữ hoa, một chữ thường, một số và một ký tự đặc biệt")]
         public string NewPassword { get; set; } = string.Empty;
         public string ConfirmPassword { get; set; } = string.Empty;
     }
