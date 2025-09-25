77d15ff9a9d782196f93f888a0bd8807e6a4506e
SangDevK18 <sanghn1306@gmail.com>
2025-09-25 22:35:41 +0700

    delete unnecessary import

--- frontend-client/src/components/layouts/layout/MainLayout.tsx
+++ frontend-client/src/components/layouts/layout/MainLayout.tsx
@@ -1,27 +1,9 @@
 import { Outlet } from 'react-router-dom'
 import { Layout } from 'antd'
-import Cookies from 'js-cookie'
-import { useNavigate } from 'react-router-dom'
 const { Content } = Layout
-import { useEffect } from 'react'
 import NavBar from '../navbar/NavBar'
 import Footer from '../../footer/Footer'
 function MainLayout() {
-    const navigate = useNavigate()
-
-    const userData = Cookies.get('userData')
-        ? JSON.parse(Cookies.get('userData') as string)
-        : null
-
-    useEffect(() => {
-        if (userData) {
-            if (userData.Role === 'Customer') {
-                if (!userData?.PhoneNumber || !userData?.Address) {
-                    navigate('/force-update')
-                }
-            }
-        }
-    }, [userData, navigate])
 
     return (
         <Layout
@@ -31,12 +13,7 @@ function MainLayout() {
             }}
         >
             <NavBar />
-            {/* <AppHeader /> */}
             <Content
-            // style={{
-            //     padding: '100px',
-            //     // paddingTop: '30px',
-            // }}
             >
                 <Outlet />
             </Content>
