43d37fcf4bd7f5217384d950ecb8f6a363add2fe
SangDevK18 <sanghn1306@gmail.com>
2025-09-25 21:08:15 +0700

    import image to header

--- frontend-client/src/components/home-page/HomePage.tsx
+++ frontend-client/src/components/home-page/HomePage.tsx
@@ -1,9 +1,14 @@
 import Slogan from "../slogan/Slogan";
-
+import headerImg from '../../assets/header.jpg';
 const HomePage: React.FC = () => {
     return (
         <div>
             <Slogan />
+            <div
+                style={{ marginTop: '20px' }}>
+                <img src={headerImg} alt="Description of image"
+                    style={{ height: "30%" }} />
+            </div>
         </div>
     )
 }
