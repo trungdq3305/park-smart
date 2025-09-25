717db3e9e66ebad1d3cbf3511750122ea8a279e2
SangDevK18 <sanghn1306@gmail.com>
2025-09-25 22:26:20 +0700

    add header and css

--- frontend-client/src/components/home-page/HomePage.tsx
+++ frontend-client/src/components/home-page/HomePage.tsx
@@ -1,13 +1,23 @@
 import Slogan from "../slogan/Slogan";
-import headerImg from '../../assets/header.jpg';
+import headerImg from '../../assets/5218594.jpg';
+import './HomePage.css';
+
 const HomePage: React.FC = () => {
     return (
         <div>
             <Slogan />
-            <div
-                style={{ marginTop: '20px' }}>
-                <img src={headerImg} alt="Description of image"
-                    style={{ height: "30%" }} />
+            <div className="homepage-header">
+                <img
+                    src={headerImg}
+                    alt="Header"
+                    className="homepage-header-img"
+                />
+                <div className="homepage-header-overlay">
+                    <h2>Finding a place to park your car ?</h2>
+                    <button>
+                        Download now
+                    </button>
+                </div>
             </div>
         </div>
     )
