e49bc1587c7bd9cbf3609144cd3d4844c80518a2
SangDevK18 <sanghn1306@gmail.com>
2025-10-06 14:52:11 +0700

    call API for term and policies

--- frontend-client/src/components/home-page/HomePage.tsx
+++ frontend-client/src/components/home-page/HomePage.tsx
@@ -1,8 +1,20 @@
 import Slogan from '../slogan/Slogan'
 import HeaderBanner from '../header-banner/HeaderBanner'
 import './HomePage.css'
+import type { TermPolicy } from '../../types/termPolicty'
+import { useGetTermsPoliciesQuery } from '../../features/terms-policies/termsAPI'
+interface TermsPoliciesResponse {
+data :{
+  data : TermPolicy[]
+}
+isLoading: boolean
+}
 
 const HomePage: React.FC = () => {
+
+const { data , isLoading } = useGetTermsPoliciesQuery<TermsPoliciesResponse>({})
+const termPoliciesData = data?.data || []
+console.log(termPoliciesData)
   return (
     <div className="homepage-container">
       <Slogan />
---
+++ frontend-client/src/types/termPolicty.ts
@@ -0,0 +1,10 @@
+export interface TermPolicy {
+    id: string
+    title: string
+    content: string
+    description: string
+    createdAt: string
+    updatedAt: string
+    createdBy: string
+    updatedBy: string
+}   
