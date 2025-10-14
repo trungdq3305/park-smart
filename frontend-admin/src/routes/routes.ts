2d862d3731890579eb14c40b8ce8ed5df86930dd
SangDevK18 <sanghn1306@gmail.com>
2025-10-14 22:17:14 +0700

    create manage account page

--- frontend-admin/src/routes/routes.ts
+++ frontend-admin/src/routes/routes.ts
@@ -3,7 +3,7 @@ import type { LayoutRoute } from '../types/Route'
 
 // Các layout này được export dưới dạng export default
 const MainLayout = lazy(() => import('../components/layouts/layout/MainLayout'))
-
+const AdminLayout = lazy(() => import('../components/layouts/layout/AdminLayout'))
 // --- Các Trang (tải lười - lazy loading) ---
 const LoginPage = lazy(() => import('../pages/LoginPage'))
 const ManageAccountPage = lazy(() => import('../pages/admin/ManageAccountPage'))
@@ -16,9 +16,15 @@ const routes: LayoutRoute[] = [
         path: '/login',
         component: LoginPage,
       },
+    ],
+  },
+  {
+    layout: AdminLayout,
+    data: [
       {
         path: '/',
         component: ManageAccountPage,
+        role: ['Admin'],
       },
     ],
   },
