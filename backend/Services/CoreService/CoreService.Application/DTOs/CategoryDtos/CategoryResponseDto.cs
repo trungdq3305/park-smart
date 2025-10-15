aa4fdd1f2b9090deb2be81231bbde35c849eebbc
trungdq3305 <fccdotm1@gmail.com>
2025-10-15 20:43:18 +0700

    Report, Category

---
+++ backend/Services/CoreService/CoreService.Application/DTOs/CategoryDtos/CategoryResponseDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreService.Application.DTOs.CategoryDtos
+{
+    public class CategoryResponseDto
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
