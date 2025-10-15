c1cd2c46dc1c0cf43dd45f3ed0cea8dc0c9bb58a
SangDevK18 <sanghn1306@gmail.com>
2025-10-15 14:49:24 +0700

    operator type

---
+++ frontend-admin/src/types/Operator.ts
@@ -0,0 +1,10 @@
+export interface Operator {
+  _id: string
+  accountId: string
+  addressId : string
+  fullName: string
+  taxCode: string
+  companyName: string
+  contactEmail: string
+  isVerified: boolean
+}
